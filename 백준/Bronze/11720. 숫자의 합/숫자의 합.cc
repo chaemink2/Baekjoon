7b0a22336cbf49bc06b1025cc81ca85ca3446a39
채민기 <109517707+chaemink2@users.noreply.github.com>
2022-09-17 17:09:39 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int n; string str;
+	cin >> n >> str;
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += (str[i] - 48);
+	}
+	cout << sum;
+	return 0;
+}
