1e3c710ba13d0791073110758a4c211cdb6ff37e
채민기 <109517707+chaemink2@users.noreply.github.com>
2022-10-01 18:01:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,25 +1,28 @@
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-unordered_map <int, int> h;
+int arr[500001];
 int N, M, card;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-    
+
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> card;
-		h[card]++;
+	for (int i = 0; i < N; i++) 
+	{
+		cin >> arr[i];
 	}
-    
+
+	sort(arr, arr + N);
+
 	cin >> M;
-	for (int i = 0; i < M; i++) {
+	for (int i = 0; i < M; i++) 
+	{
 		cin >> card;
-		cout << h[card] << " ";
+		cout << upper_bound(arr, arr + N, card) - lower_bound(arr, arr + N, card) << " ";
 	}
-    return 0;
+	return 0;
 }
