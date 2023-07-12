380dd1c385c4c3d11808ec0e9f675bae3619e91e
채민기 <109517707+chaemink2@users.noreply.github.com>
2023-07-12 21:55:25 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		string str;
+
+		getline(cin, str);
+
+		cout << str << '\n';
+	}
+	return 0;
+}
