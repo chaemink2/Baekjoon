303b75731e75a769972fcb673d0e719ada8ba26f
채민기 <109517707+chaemink2@users.noreply.github.com>
2025-08-17 00:34:18 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <queue>
+
+using namespace std;
+
+long long solution(int n)
+{
+    long long answer = 0;
+    int dp[2001] = {};
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i =2; i<=n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    return dp[n];
+}
